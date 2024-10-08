6c3b29dd1c3e6a07a3497cbd6749e49d74c11fe7
Radhika426 <radhikably426@gmail.com>
2024-10-08 11:12:21 +0530

    first push

---
+++ SeleniumSampleProject/src/test/java/pac2/OrgTest.java
@@ -0,0 +1,16 @@
+package pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+	@Test
+	public void createOrgTest() {
+		System.out.println("createOrgTest executed");
+	}
+	
+	@Test
+	public void modifyOrgTest() {
+		System.out.println("modifyOrgTest executed");
+	}
+}
